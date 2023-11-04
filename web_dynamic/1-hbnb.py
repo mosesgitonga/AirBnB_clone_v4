630641851b763fb940cbeeadff70d079da1d8129
mosesgitonga <infosec947@gmail.com>
2023-11-04 09:46:12 +0300

    more canges

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -6,11 +6,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -22,27 +22,19 @@ def close_db(error):
 @app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-    cache_id = str(uuid.uuid4())
-
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
     return render_template('1-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
                            places=places,
-                           cache_id=cache_id)
-
+                           users=users)
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
