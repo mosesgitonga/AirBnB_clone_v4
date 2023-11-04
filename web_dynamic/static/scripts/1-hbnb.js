53325908312efdeef90e71f06ae6809899f06c11
mosesgitonga <infosec947@gmail.com>
2023-11-04 07:23:22 +0300

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,15 @@
-$(function() {
-    const selectedAmenities = {};
-    $(".amenity").change(function() {
-        var amenityId = $(this).data('amenity-id');
-        
-        if ($(this).prop('checked')) {
-            selectedAmenities[amenityId] = true;
-        } else {
-            delete selectedAmenities[amenityId];
-        }
-        
-        var selectedAmenitiesText = Object.keys(selectedAmenities).map(function(amenityId) {
-            return "Amenity " + amenityId;
-        }).join(', ');
-
-        $('.Amenities h4').text("Selected Amenities: " + selectedAmenitiesText);
-    });
+window.addEventListener('load', function () {
+  const amenityIds = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
+  });
 });
